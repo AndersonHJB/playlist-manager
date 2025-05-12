2349b003a72e3ad3bda1ac8f6c646b245c8cfd7d
AndersonHJB <bornforthis@bornforthis.cn>
2025-05-12 10:56:36 +0800

    Add .gitignore and initial playlist-manager.py file

---
+++ playlist-manager.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+# @Time    : 2025/5/12 10:56
+# @Author  : AI悦创
+# @FileName: playlist-manager.py
+# @Software: PyCharm
+# @Blog    ：https://bornforthis.cn/
+# code is far away from bugs with the god animal protecting
+#    I love animals. They taste delicious.
